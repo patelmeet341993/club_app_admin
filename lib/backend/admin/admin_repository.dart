600c5de4f0bc853b6ce7df7abe7c1a20a5e50429
DELL <virendesaivd77@gmail.com>
2023-05-08 12:29:38 +0530

    Admin Related set up

---
+++ lib/backend/admin/admin_repository.dart
@@ -0,0 +1,32 @@
+
+import 'package:club_model/configs/constants.dart';
+import 'package:club_model/configs/typedefs.dart';
+import 'package:club_model/utils/my_print.dart';
+
+import '../../models/admin/property_model.dart';
+
+class AdminRepository {
+  Future<PropertyModel?> getPropertyData() async {
+    MyPrint.printOnConsole("AdminRepository().getPropertyData() called");
+
+    PropertyModel? propertyModel;
+
+    try {
+      MyFirestoreDocumentSnapshot snapshot = await FirebaseNodes.adminPropertyDocumentReference.get();
+      MyPrint.printOnConsole("snapshot exist:${snapshot.exists}",);
+      MyPrint.printOnConsole("snapshot data:${snapshot.data()}",);
+
+      if(snapshot.data() != null && (snapshot.data()?.isNotEmpty ?? false) ) {
+        propertyModel = PropertyModel.fromMap(snapshot.data()!);
+      }
+    }
+    catch(e, s) {
+      MyPrint.printOnConsole("Error in getting PropertyModel in AdminRepository().getPropertyData():$e",);
+      MyPrint.printOnConsole(s,);
+    }
+
+    MyPrint.printOnConsole("Final propertyModel:$propertyModel",);
+
+    return propertyModel;
+  }
+}
