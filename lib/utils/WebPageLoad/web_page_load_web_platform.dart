4363a22cd24e862e8e5641195c3400f75427ac84
Dishant <agrawaldishant29@gmail.com>
2023-09-22 12:45:01 +0530

    added web_page_load backend in utils

---
+++ lib/utils/WebPageLoad/web_page_load_web_platform.dart
@@ -0,0 +1,28 @@
+import 'dart:html' deferred as html;
+
+import 'package:intl/intl.dart';
+
+import '../my_print.dart';
+import '../parsing_helper.dart';
+
+Future<void> checkPageLoad() async {
+  MyPrint.printOnConsole("Loading Library:${DateFormat("dd/MM/yyyy hh:mm:ss.SSS aa").format(DateTime.now())}");
+  await html.loadLibrary();
+  MyPrint.printOnConsole("Loaded Library:${DateFormat("dd/MM/yyyy hh:mm:ss.SSS aa").format(DateTime.now())}");
+
+  var myElement = html.querySelector('#datetime');
+  String? innerText = myElement?.innerHtml;
+  MyPrint.printOnConsole("myElement:$myElement");
+  MyPrint.printOnConsole("innerText:$innerText");
+
+  DateTime? parsedHtmlTime = ParsingHelper.parseDateTimeMethod(innerText);
+  MyPrint.printOnConsole("parsed datetime:${parsedHtmlTime != null ? DateFormat("dd/MM/yyyy hh:mm:ss.SSS aa").format(parsedHtmlTime) : null}");
+
+  DateTime now = DateTime.now();
+  MyPrint.printOnConsole("Current Time in main.dart:${DateFormat("dd/MM/yyyy hh:mm:ss.SSS aa").format(now)}");
+
+  if (parsedHtmlTime != null) {
+    Duration difference = now.difference(parsedHtmlTime);
+    MyPrint.printOnConsole("Took ${difference.inMilliseconds / 1000} Seconds to Load Dart Code");
+  }
+}
